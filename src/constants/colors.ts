a610c86ee9749817f515cd499d61afd75ba26d48
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-26 17:43:35 -0300

    feat(Colors): Criação de arquivo de cores

---
+++ src/constants/colors.ts
@@ -0,0 +1,19 @@
+const primary = '#1547ff';
+const secondary = '#ffbb00';
+
+export default {
+  colors: {
+    primary: primary,
+    accent: secondary,
+    grey: '#555555'
+  },
+  light: {
+    background: '#d5d5d5',
+    text: '#333',
+    accentText: '#FFF',
+    boxBackground: '#FFF',
+    header: primary,
+    statusbar: '#0537aa',
+    links: primary
+  }
+};
