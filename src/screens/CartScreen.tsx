df346ab81c5b552710336ed7f540a405416d635d
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-26 21:31:52 -0300

    feat(Cart): Soma total no carrinho

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import ProductList from '../components/ProductList';
 
+import ProductList from '../components/ProductList';
+import colors from '../constants/colors';
+import currencyFormat from '../utils/currencyFormat';
 import { IProduct } from '../utils/types';
 import products from '../__mocks__/products';
 
 const CartScreen = () => {
   type IProductList = { item: IProduct };
+  const totalQuantity = products.length;
+  const textPluralSingular = totalQuantity > 1 ? 'produtos' : 'produto';
   const renderProduct = ({ item }: IProductList) => {
     return <ProductList item={item} />;
   };
 
+  const totalPrice = () => {
+    let sum = 0;
+    for (let i = 0; i < totalQuantity; i++) {
+      sum = sum + products[i].price;
+    }
+
+    return currencyFormat(sum);
+  };
+
   return (
-    <FlatList
-      data={products}
-      renderItem={renderProduct}
-      style={styles.container}
-    />
+    <>
+      <Text
+        style={styles.title}
+      >{`${totalQuantity} ${textPluralSingular} - ${totalPrice()}`}</Text>
+      <FlatList
+        data={products}
+        renderItem={renderProduct}
+        style={styles.container}
+      />
+    </>
   );
 };
 
@@ -26,4 +44,9 @@ const styles = StyleSheet.create({
   container: {
     padding: 10
   },
+  title: {
+    fontSize: 18,
+    margin: 10,
+    color: colors.light.text
+  }
 });
